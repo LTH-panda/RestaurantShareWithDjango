6bc6e0da373d6356eb0db986808b608f4ba28d01
이태현 <johtta888@naver.com>
2021-01-23 12:51:40 +0000

    2021-01-23-#03

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('restaurantCreate/', views.create_restaurant, name = 'create_restaurant'),
-    path('restaurantDetail/', views.detail_restaurant, name = 'detail_restaurant'),
+    path('restaurantCreate/', views.restaurantCreate, name = 'restaurantCreate'),
+    path('restaurantCreate/create', views.create_restaurant, name = 'create_restaurant'),
+    path('restaurantDetail/', views.restaurantDetail, name = 'restaurantDetail'),
     path('categoryCreate/', views.categoryCreate, name = 'categoryCreate'),
     path('categoryCreate/create', views.create_category, name = 'create_category'),
+    path('categoryCreate/delete', views.delete_category, name = 'delete_category'),
     
 ]
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -6,7 +6,9 @@ from .models import *
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
-    content = {'categories' : categories}
+    restaurants= Restaurant.objects.all()
+    content = {'categories' : categories,
+               'restaurants' : restaurants}
     return render(request, 'shareRes/index.html', content)
 
 def categoryCreate(request):
@@ -16,13 +18,32 @@ def categoryCreate(request):
 
 def create_category(request):
     category_name = request.POST['categoryName']
-    new_category = Category(category_name = category_name)
+    new_category = Category(category_name = category_name) # 생성자  : 클래스 명(인수)
     new_category.save()
     return HttpResponseRedirect(reverse('index'))
     # return HttpResponse("create category")
+    
+def delete_category(request):
+    category_id = request.POST['categoryId']
+    delete_category = Category.objects.get(id = category_id) # 데이터 불러올 때 : 클래스.objects.get(id)
+    delete_category.delete()
+    return HttpResponseRedirect(reverse('categoryCreate'))
+
+def restaurantCreate(request):
+    categories = Category.objects.all()
+    content = {'categories' : categories}
+    return render(request, 'shareRes/restaurantCreate.html', content)
 
 def create_restaurant(request):
-    return render(request, 'shareRes/restaurantCreate.html')
+    category_id = request.POST['resCategory']
+    category = Category.objects.get(id = category_id)
+    title = request.POST['resTitle']
+    link = request.POST['resLink']
+    content = request.POST['resContent']
+    keyword = request.POST['resLoc']
+    new_res = Restaurant(category = category, restaurant_name = title, restaurant_link = link, restaurant_content = content, restaurant_keyword = keyword)
+    new_res.save()
+    return HttpResponseRedirect(reverse('index'))
 
-def detail_restaurant(request):
+def restaurantDetail(request):
     return render(request, 'shareRes/restaurantDetail.html')
